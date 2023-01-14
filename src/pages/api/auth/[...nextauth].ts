5d7d977d04a68bce6914849857da9251c26eed78
NicolasJandre <nicolasjandre@live.com>
2023-01-13 23:08:44 -0300

    stripe api configured, subscribe button feature added

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
+
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
@@ -9,6 +12,35 @@ export const authOptions = {
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"), 
+                  q.Casefold(email as string)
+                  )
+                ) 
+              ),
+              q.Create(
+                q.Collection("users"), {data: { email }}
+          ),
+            q.Get(q.Match(q.Index("user_by_email"),
+              q.Casefold(email as string))
+            )
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 }
 
 export default NextAuth(authOptions)
