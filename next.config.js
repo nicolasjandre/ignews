fd8da748db0c48d8da43c98258766cfd4ae7ce4e
Nicolas <nicolasjandre@live.com>
2023-03-05 11:48:56 -0300

    preparing deploy

--- next.config.js
+++ next.config.js
@@ -4,17 +4,16 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Define the Content Security Policy header
+        // Define the Content Security Policy header for Stripe
         source: '/(.*)',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "connect-src 'self' https://checkout.stripe.com; frame-src 'self' https://checkout.stripe.com; script-src 'self' https://checkout.stripe.com; img-src 'self' https://*.stripe.com;",
+            value:
+              "connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://checkout.stripe.com; frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://checkout.stripe.com https://connect-js.stripe.com; script-src 'self' https://js.stripe.com https://maps.googleapis.com https://checkout.stripe.com https://connect-js.stripe.com; img-src 'self' https://*.stripe.com;",
           },
         ],
       },
     ];
   },
 };
-
-module.exports = nextConfig
