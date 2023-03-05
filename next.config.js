e85b66edc3c83dbe9b8eac1d668eeb9bb2acca6f
Nicolas <nicolasjandre@live.com>
2023-03-05 11:40:32 -0300

    preparing deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Define the Content Security Policy header
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "connect-src 'self' https://checkout.stripe.com; frame-src 'self' https://checkout.stripe.com; script-src 'self' https://checkout.stripe.com; img-src 'self' https://*.stripe.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
