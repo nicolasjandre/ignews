e2885ac9c1a730f38299ce3a3226f2bbf19a1aef
Nicolas <nicolasjandre@live.com>
2023-03-05 10:24:29 -0300

    preparing deploy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,7 +21,6 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      scope: "read:user",
     }),
   ],
   callbacks: {
